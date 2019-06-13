5c18ab3f1c21a3fd1cfd4974b268ee5ac288b9c9
alakaktyc <40630262+alakaktyc@users.noreply.github.com>
2019-06-14 00:41:45 +0300

    allnew

--- assets/js/menu.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $(window).scroll(function() {
-        let top = $(document).scrollTop();
-        if (top < 57) $('.header__navigation').css({top: '0', position: 'relative'});
-        else $('.header__navigation').css({top: '0', position: 'fixed',width:'100%',minWidth:'980px'});
-    });
-});
